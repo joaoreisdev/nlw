8791fed53058860b225ad90b1ffc8f8764396371
João <joaovictorcruzereis@gmail.com>
2020-08-01 19:52:14 -0300

    Inserindo Js para polular campos de Estado/Cidade

---
+++ scripts/intro.js
@@ -0,0 +1,73 @@
+// Comments
+// document.write("Hello")
+
+// Variaveis, tipo de dados
+// let myvar = "HEY"
+// const myconst = "HI"
+
+// document.write(myconst+myvar)
+
+// String
+// const s1 = "Isso é uma String"
+// const s2 = 'Isso é uma String2'
+// const s3 = `Isso é uma String3`
+
+// document.write(s3)
+
+// Number
+// const n1 = 6
+// const n2 = 10
+// const n3 = '70'
+// document.write(n1 + n2 + n3)
+
+//Boolean - True or False
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bFalse)
+
+//Estruturas de dados
+//Objeto, todos possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: '1,80m',
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return 'Run Forest'
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+// Array - Vetores(São coleções da dados)
+//[ ... ]
+// const colecaoDeBolinhas = ['Blue', 'Green', 1, {name:'MyName'}]
+
+// document.write(colecaoDeBolinhas[1])
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades
+// function sayMyName(name){
+  
+//   document.write(name)
+  
+// }
+
+// sayMyName('Douglas')
+// sayMyName('Joao')
+// sayMyName('Matheus')
+
+// Condicionais
+// const notaFinal = 6
+
+// if( notaFinal >= 7 ){
+//   document.write('Aprovado')
+// } else {
+//   document.write('Reprovado')
+// }
+
+//Loop repeticoes
+// for(i=0; i <10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
